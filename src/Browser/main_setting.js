f7350fbcf8d077febc427169f2348e1225fb18b7
teddy1565 <teddy1565@gmail.com>
2020-12-28 14:49:07 +0800

    latest

--- src/Browser/main_setting.js
+++ src/Browser/main_setting.js
@@ -6,11 +6,7 @@ function RenderExteriorAttributes(){
         <thead class="thead-dark">
             <tr id="topic">
                 <th scope="col">#</th>
-                <th class="bs-checkbox" data-field="state">
-                    <label>
-                        <input type="checkbox" id="SelectAllCheckBox" onclick="SelectAllCheckBox()">
-                    </label>
-                </th>
+                <th scope="col">操作</th>
             </tr>
         </thead>
         <tbody id="WindowList" data-link="row">
@@ -34,19 +30,66 @@ window.ipcRenderer.receive("MainSettingWindowDashBoardManager",(args)=>{
             document.getElementById("WindowList").innerHTML+=`
             <tr>
                 <th scope="row">${i}</th>
-                <td class="bs-checkbox">
+                <td class="btn" id="operator_${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}">
                     <label>
-                        <input data-index="${i}" id="RenderExteriorAttributesCheckBoxItem" name="btSelectItem" type="checkbox" value="0">
+                        <button type="button" class="btn btn-success" onclick="modifyExteriorAttributes('${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}')">修改</button>
+                    </label>
+                    <label>
+                        <button type="button" class="btn btn-danger" onclick="deleteExteriorAttributes('${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}')">刪除</button>
                     </label>
                 </td>
-                <td>${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}</td>
-                <td>${args.data[i].width}</td>
-                <td>${args.data[i].height}</td>
+                <td id="ID_${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}">${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}</td>
+                <td id="width_${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}">${args.data[i].width}</td>
+                <td id="height_${args.data[i].ID==undefined?"PublicConfig":args.data[i].ID}">${args.data[i].height}</td>
             </tr>
             `;
         }
+        document.getElementById("WindowList").innerHTML+=`
+        <tr>
+            <th scope="row"></th>
+            <td class="btn">
+                <label>
+                    <button type="button" class="btn btn-warning">新增視窗外觀屬性</button>
+                </label>
+            </td>
+            <td>===</td>
+            <td>===</td>
+            <td>===</td>
+        </tr>
+        `;
     }
 });
+function modifyExteriorAttributes(id){
+    document.getElementById(`operator_${id}`).innerHTML=`
+        <label>
+            <button type="button" class="btn btn-warning" onclick="saveExteriorAttributes('${id}')">儲存</button>
+        </label>
+    `;
+    let windowID = document.getElementById(`ID_${id}`).innerHTML;
+    let width = document.getElementById(`width_${id}`).innerHTML;
+    let height = document.getElementById(`height_${id}`).innerHTML;
+    document.getElementById(`ID_${id}`).className="text";
+    document.getElementById(`ID_${id}`).innerHTML=`
+    <label>
+        <input type="text" class="text" value="${windowID}">
+    </label>
+    `;
+    document.getElementById(`width_${id}`).className="text";
+    document.getElementById(`width_${id}`).innerHTML=`
+    <label>
+        <input type="text" class="text" value="${width}">
+    </label>
+    `;
+    document.getElementById(`height_${id}`).className="text";
+    document.getElementById(`height_${id}`).innerHTML=`
+    <label>
+        <input type="text" class="text" value="${height}">
+    </label>
+    `;
+}
+function deleteExteriorAttributes(id){
+
+}
 function SelectAllCheckBox(btnID){
 
 }
